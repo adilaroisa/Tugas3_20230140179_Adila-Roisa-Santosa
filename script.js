73294c469c5a3c16cab3a328f6e9f26d4ecf065a
adilaroisa <adilaroisa688@gmail.com>
2025-03-26 22:11:22 +0700

    perbaikan

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 // Fungsi untuk menampilkan alert
 function tampilkanAlert() {
-    alert("Halo! Ini percobaan alert pakai JavaScript.");
+    alert("Data berhasil disimpan! Pengisian tabel telah sukses.");
 }
 
-// Fungsi untuk menampilkan waktu saat ini dengan format yang diminta
 function tampilkanWaktu() {
     let waktuSekarang = new Date().toString();
     document.getElementById("waktu").textContent = "Tanggal dan waktu saat ini: " + waktuSekarang;
